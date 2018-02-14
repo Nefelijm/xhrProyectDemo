b5bb7ea1a7345797f06489452259db8a678d973f
Nefelijm <sistemasnik20@gmail.com>
2018-02-13 20:20:40 -0500

    llamando funcion addNew

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
+
 //Elementos del DOM que utilizaremos
-const form = document.getElementById('search');
+const form = document.getElementById('search-form');
 const label = document.getElementById('search-label');
 const input = document.getElementById('search-keyword');
 const inputBtn = document.getElementById('submit-btn');
@@ -8,8 +9,8 @@ const ul = document.getElementById('response-container');
 let  searchText ;
 
 //realizando el evento y dentro de ella llamamos a la funcion getNews
-form.addEventListener('submit',function(event){
-event.preventDefault;
+form.addEventListener('submit',function(e){
+event.preventDefault();
 div.innerHTML = '';
 searchText = input.value;
 getNew();
@@ -20,7 +21,7 @@ function getNew() {
 //Instanciando nuestro objeto
 const articleRequest = new XMLHttpRequest();
 //Abriendo la peticion
-    articleRequest.open('GET','https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=91e23984f8e5409081ea47215e9e61c3');
+    articleRequest.open('GET',`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=91e23984f8e5409081ea47215e9e61c3`);
 //onload cuando la peticion esta con exito
     articleRequest.onload = addNews;
 //onerror cuando la peticion falla
@@ -28,4 +29,23 @@ const articleRequest = new XMLHttpRequest();
 //Enviando las peticiones
     articleRequest.send();
 }
+
+//Creamos  la funcion handleError()
+function handleError(){
+console.log('Se ha presentado un error');
+}
+
+function addNews(){
+ const data = JSON.parse(this.responseText);
+ const article = data.response.docs[0];
+ const title = article.headline.main;
+ const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.innerText = snippet;
+
+  div.appendChild(li);
+}
+
+
 
