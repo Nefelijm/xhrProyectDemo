9737757b31e7564150ccc068f235d03608c04409
Nefelijm <sistemasnik20@gmail.com>
2018-02-13 19:48:30 -0500

    llamando funcion getnew

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,17 @@ searchText = input.value;
 getNew();
 });
 
-
+//Funcion donde crearemos nuestra peticiones
 function getNew() {
-
+//Instanciando nuestro objeto
+const articleRequest = new XMLHttpRequest();
+//Abriendo la peticion
+    articleRequest.open('GET','https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=91e23984f8e5409081ea47215e9e61c3');
+//onload cuando la peticion esta con exito
+    articleRequest.onload = addNews;
+//onerror cuando la peticion falla
+    articleRequest.onerror = handleError;
+//Enviando las peticiones
+    articleRequest.send();
 }
+
